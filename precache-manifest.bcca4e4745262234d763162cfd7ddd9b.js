ba254bb036b60ad131f517b6a2ee87318f360742
Nate <nate@pop-os.localdomain>
2020-09-09 14:57:21 +0100

    Updates

---
+++ precache-manifest.bcca4e4745262234d763162cfd7ddd9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a651172914f88330a028cc1c2839663",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66f14db48fab96647d46",
+    "url": "/static/css/main.e94b1f7f.chunk.css"
+  },
+  {
+    "revision": "4bbd47e4f5e60ab2de0e",
+    "url": "/static/js/2.fc33b61c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fc33b61c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66f14db48fab96647d46",
+    "url": "/static/js/main.87978999.chunk.js"
+  },
+  {
+    "revision": "43a762a161abd80b9684",
+    "url": "/static/js/runtime-main.30dae16b.js"
+  }
+]);
